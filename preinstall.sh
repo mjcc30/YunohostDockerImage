3d4afa42de99a01ae129d17dd548a2395de3a975
mjcc30 <cordeiromaxime@gmail.com>
2020-11-09 07:46:06 +0100

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -28,13 +28,48 @@ git -C /tmp/install_script checkout $BRANCHE_TYPE
 # hack YunoHost install_script for bypass systemd check
 sed -i "s@/run/systemd/system@/run@g" /tmp/install_script/install_yunohost
 
+# debug systemctl issues for docker, add proxy
+mv /bin/systemctl /bin/systemctl.bin
+echo -e "#"'!'"/bin/sh\n/bin/./systemctl.bin\nexit 0\n" > /bin/systemctl
+chmod +x /bin/systemctl
+
+# force ulimit for slapd now
+ulimit -n 1024
+
 # do yunohost installation
 cd /tmp/install_script
-./install_yunohost -a -d $INSTALL_TYPE
-[ "$?" != "0" ] && cat /var/log/yunohost-installation.log && cat /var/log/daemon.log && echo "error while yunohost installation" && exit 1
+./install_yunohost -f -a -d $INSTALL_TYPE
+[ "$?" != "0" ] && echo "error while yunohost installation" && exit 1
 
-# force ulimit for slapd
+# hack iptables for yunohost in docker
+mv /usr/sbin/iptables /usr/sbin/iptables.ori
+echo -e "#"'!'"/bin/sh\necho \"fake iptables for yunohost inside docker, unusable. For return non failure unix code 0. Bye !\"\nexit 0\n" > /usr/sbin/iptables
+chmod +x /usr/sbin/iptables
+
+# remove proxy for systemctl
+rm -f /bin/systemctl
+mv /bin/systemctl.bin /bin/systemctl
+
+# force ulimit for slapd in starting script
 sed -i '/\/lib\/lsb\/init-functions/a ulimit -n 1024' /etc/init.d/slapd
+
+# patchs for yunohost
+adduser admin
+systemctl enable nginx
+systemctl enable yunohost-api
+systemctl enable php7.3-fpm
+systemctl enable fail2ban
+[ ! -f /etc/fail2ban/filter.d/sshd-ddos.conf ] \
+	&& echo -e "[Definition]\n" > /etc/fail2ban/filter.d/sshd-ddos.conf
+[ ! -f /etc/fail2ban/filter.d/postfix-sasl.conf ] \
+        && echo -e "[Definition]\n" > /etc/fail2ban/filter.d/postfix-sasl.conf
+touch /var/log/mail.log
+#systemctl enable dovecot
+#systemctl enable postfix
+#systemctl enable rspamd
+#systemctl enable avahi-daemon
+#systemctl enable dnsmasq
+#systemctl enable redis-server
 
 # cleaning
 apt-get clean
